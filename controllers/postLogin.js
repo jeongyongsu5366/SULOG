29a8f6cfd090c317b9f7e562caf57023ff9b192f
su <jeongyongsu5366@gmail.com>
2019-07-25 04:20:55 +0900

    Add getLogin and postLogin controllers
    - getLogin controller is used for providing login form for user
    - postLogin is used for validating client's input and if it is valid, it
    will create jwt token for later use.

---
+++ controllers/postLogin.js
@@ -0,0 +1,26 @@
+const { localUser, loginValidation } = require('../database/models/LocalUser');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const Post = require('../database/models/Post');
+
+module.exports = async function(req, res) {
+	const { error } = loginValidation(req.body);
+	if (error) return res.status(400).send(error.details[0].message);
+
+	const user = await localUser.findOne({ email: req.body.email });
+	if (!user) return res.status(400).send('Email or Passowrd is wrong');
+	// Check if password is correct
+	const validPassword = await bcrypt.compare(req.body.password, user.password);
+	if (!validPassword) return res.status(400).send('Invalid Password');
+
+	const payload = {
+		_id: user._id,
+		isAdmin: user.isAdmin,
+		email: user.email
+	};
+	const token = jwt.sign(payload, process.env.SECRET_KEY, { expiresIn: 86400 });
+	res.cookie('auth_token', token, { maxAge: 86400, httpOnly: true });
+	console.log(user);
+	const posts = await Post.find({});
+	res.status(200).render('homepage', { posts });
+};
